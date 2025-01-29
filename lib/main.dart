ffcd50fa6dbe3b9f82c42967165681a9ddb124b3
SamuelMathewCSULB <Samuel.Mathew@student.csulb.edu>
2025-01-28 22:29:43 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,15 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  FirebaseOptions options = FirebaseOptions(
+    apiKey: "AIzaSyA_9SV39BIwuOQULX_mUp0w3c2KdEU5oJ8",
+    appId: "1:13701743979:android:1b0281e61059ce0eb0b82e",
+    projectId: "flutter-expense-tracker-a6400",
+    messagingSenderId: "13701743979",
+    storageBucket: "flutter-expense-tracker-a6400.firebasestorage.app",
+  );
+  await Firebase.initializeApp(options: options);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
